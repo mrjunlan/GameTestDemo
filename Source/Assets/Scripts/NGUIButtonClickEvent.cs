9f75b3ec6011cbdd7348d77e52d406b4f399993d
mrjunlan <504800903@qq.com>
2017-04-02 14:01:20 +0800

    添加游戏素材
    加入了游戏需要的素材，角色图片，动画图片，音效

---
+++ Source/Assets/Scripts/NGUIButtonClickEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NGUIButtonClickEvent : MonoBehaviour {
+
+    public void L_Button_Click() {
+        print("L_button_clicked");
+    }
+
+
+    public void S_Button_Click() {
+        print("S_button_clicked");
+    }
+}
